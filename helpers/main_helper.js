5e37b04e04d865a7582f6e54732a8dd2aadca761
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-10-03 12:43:01 +0530

    🆘 added invite helper function

---
+++ helpers/main_helper.js
@@ -0,0 +1,25 @@
+const User = require("../models/user");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+
+class MainHelper {
+    static async sendInvite(mobile) {
+        const password = crypto.randomBytes(4).toString('hex');
+
+        if (await User.exists({ mobile })) {
+            return false;
+        }
+        const user = await User.create({
+            name: "New User",
+            mobile,
+            role: "string",
+            password: await bcrypt.hash(password, parseInt(process.env.HASH_ROUNDS))
+        });
+
+        // TODO: Send mail / OTP
+
+        return user;
+    }
+}
+
+module.exports = MainHelper;
