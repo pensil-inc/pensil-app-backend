87d9ab0609309757475acb541b4790a2e6108709
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-10-07 23:00:19 +0530

    removed webpush

--- config/webpush.js
+++
@@ -1,6 +0,0 @@
-const webpush = require('web-push');
-
-// Generate Vapid keys before using this
-webpush.setVapidDetails('mailto:chaharshubhamsingh@gmail.com', process.env.VAPID_PUBLICKEY || '', process.env.VAPID_PRIVATEKEY || '');
-
-module.exports = webpush;
