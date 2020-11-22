439325f9ad8fca11687aa47bdd4a070408dbed24
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-11-22 16:18:58 +0530

    added student id in get-all-student-list api

--- resources/student-list-resource.js
+++ resources/student-list-resource.js
@@ -4,6 +4,7 @@ module.exports = class StudentListResource extends Resource {
 
     format(resource) {
         return {
+            id: resource._id,
             mobile: resource.mobile,
             name: resource.name,
         };
--- test/teacher-batch-test.js
+++ test/teacher-batch-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index');
+const Batch = require('../models/batch');
 const user = require('../models/user');
 const TestHelper = require('./test-helper');
 const should = chai.should();
@@ -27,6 +28,9 @@ describe("Teacher Batch CRUD", function () {
             }, {
                 upsert: true
             })
+
+            // delete all the batches
+            Batch.deleteMany({});
 
             return resolve();
         });
@@ -66,6 +70,45 @@ describe("Teacher Batch CRUD", function () {
                         res.body.batch.should.have.property('id');
                         res.body.batch.classes.should.have.length(1);
                         res.body.batch.students.should.have.length(2);
+                        return resolve();
+                    });
+            })
+        });
+    });
+
+    describe("POST: /batch/:id", () => {
+        it("Teacher is able to edit batch", async () => {
+            return new Promise(async resolve => {
+                const requestBody = {
+                    name: "Test Batch 2",
+                    description: "This is some description",
+                    classes: [
+                        {
+                            "dayOfWeek": 1,
+                            "startTime": "22:00",
+                            "endTime": "24:00"
+                        }
+                    ],
+                    subject: "Hindi",
+                    students: [
+                        7017958027
+                    ]
+                };
+
+                const token = await TestHelper.getTokenUsingMobileNumber(8218578499);
+
+                chai.request(server)
+                    .post('/api/batch')
+                    .set(
+                        'Authorization',
+                        'Bearer ' + token)
+                    .send(requestBody)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('batch');
+                        res.body.batch.should.have.property('id');
+                        res.body.batch.subject = "Hindi";
+                        res.body.batch.students.should.have.length(1);
                         return resolve();
                     });
             })
